f63b264f8f87b74b4b486d3a637420b93b44b42c
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:48 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoArgs/ChangeContentArgsGitBashBuilder.cs
@@ -0,0 +1,9 @@
+namespace SunamoGitBashBuilder;
+
+
+internal class ChangeContentArgsGitBashBuilder
+{
+    internal bool removeNull = false;
+    internal bool removeEmpty = false;
+    internal bool switchFirstAndSecondArg = false;
+}
